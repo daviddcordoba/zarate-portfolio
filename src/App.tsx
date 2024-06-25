e4a41ba1c1057f32183812b94a135866282e99d9
David <cordoba_d@rocketmail.com>
2024-06-25 18:49:40 -0300

    agregando ver mas ver menos

--- src/App.tsx
+++ src/App.tsx
@@ -72,7 +72,14 @@ const TRABAJOS = [{name: 'Pieza sonora: Representacion de el silencio', data: El
 function App() {
 
   const email = 'gabzm240@gmail.com';
+  const [visibleVideos, setVisibleVideos] = useState(6); // Número inicial de videos visibles
 
+  const loadMoreVideos = () => {
+    setVisibleVideos(prev => prev + 3); // Aumentar el número de videos visibles en 4
+  };
+  const showFewerVideos = () => {
+    setVisibleVideos(prev => Math.max(prev - 3, 6)); // Reducir el número de videos visibles en 4, mínimo 4
+  };
   const [copied, setCopied] = useState(false);
 
     const copyToClipboard = () => {
@@ -173,22 +180,42 @@ function App() {
 
       </section>
 
-      <section className='my-20' id='ugc'>
+      <section className='my-20 flex flex-col' id='ugc'>
         <p className='text-center text-lg text-gray-400 '>UGC</p>
         <h1 className='text-center text-5xl mb-12 text-white'>Contenido Generado por el Usuario</h1>
 
         <div className='flex flex-wrap justify-center gap-8'>
-          {UGC_VIDEOS.map((video, index) => (
-            <div key={index} className='px-10 flex flex-col items-center'>
-              <div className='relative h-[612px] w-[305px] bg-black rounded-[60px] shadow-xl overflow-hidden border-[14px] border-black'>
-                  <div className='absolute inset-[-19.5px] h-full w-full object-cover'>
-                      <ReactPlayer url={video.video} controls width={310} height={600} />
-                  </div>
-              </div>
-              <h2  className='text-xl text-white max-w-[305px] text-center  overflow-hidden'>{video.name}</h2>
+      {UGC_VIDEOS.slice(0, visibleVideos).map((video, index) => (
+        <div key={index} className='px-10 flex flex-col items-center'>
+          <div className='relative h-[612px] w-[305px] bg-black rounded-[60px] shadow-xl overflow-hidden border-[14px] border-black'>
+            <div className='absolute inset-[-19.5px] h-full w-full object-cover'>
+              <ReactPlayer url={video.video} controls width={310} height={600} />
             </div>
-        ))}
-        </div>       
+          </div>
+          <h2 className='text-xl text-white max-w-[305px] text-center overflow-hidden'>{video.name}</h2>
+        </div>
+      ))}
+      
+      
+    </div>  
+
+    {visibleVideos < UGC_VIDEOS.length && (
+        <button 
+          onClick={loadMoreVideos} 
+          className='mt-8 px-6 py-3 mx-auto w-[25%] bg-gray-200 text-black rounded-full'
+        >
+          <span className='text-lg'>Ver mas</span>
+        </button>
+      )}  
+      {visibleVideos > 6 && (
+          <button 
+            onClick={showFewerVideos} 
+            className=' mt-4 px-6 py-3 mx-auto w-[25%] bg-gray-200 text-black  rounded-full'
+          >
+            <span className='text-lg'>Ver menos</span>
+          </button>
+        )}
+       
       </section>
 
       <section className='my-20' id='trabajos'>
